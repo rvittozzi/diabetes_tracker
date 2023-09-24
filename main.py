484c37f0e1ab2c7896eb3f32f11724c755f5ef8e
rvittozzi <137367134+rvittozzi@users.noreply.github.com>
2023-09-24 17:07:53 -0400

    edited files

--- main.py
+++ main.py
@@ -3,48 +3,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 import os
-import threading
-import time
-from plyer import notification
-from diabetes_utils import get_last_check_time
-from diabetes_data_module import get_blood_sugar_data_from_csv
 
 app = Flask(__name__)
 
-# Initialize the CSV file path
-csv_file = "diabetes_data.csv"
+# Configurations
+CSV_FILE = "diabetes_data.csv"
+ENTRIES = []
 
-# Initialize an empty list to store entries
-entries = []
 
-# Function to load existing data from the CSV file
 def load_data():
-    if os.path.exists(csv_file):
-        df = pd.read_csv(csv_file)
-        df["Date"] = pd.to_datetime(df["Date"])
-        entries.extend(df.to_dict(orient="records"))
+    global ENTRIES
+    if os.path.exists(CSV_FILE):
+        try:
+            df = pd.read_csv(CSV_FILE)
+            df["Date"] = pd.to_datetime(df["Date"])
+            ENTRIES.extend(df.to_dict(orient="records"))
+        except Exception as e:
+            print(f"An error occurred while reading the CSV: {e}")
 
-# Function to add a new entry
-def add_entry():
-    global entries
-    if request.method == "POST":
-        date_str = request.form.get("date")
-        blood_sugar = float(request.form.get("blood_sugar"))
-        date = datetime.strptime(date_str, "%Y-%m-%d")
-        entry = {"Date": date, "Blood Sugar (mg/dL)": blood_sugar}
-        entries.append(entry)
 
-        # Save the entry to the CSV file
-        df = pd.DataFrame(entries)
-        df.to_csv(csv_file, index=False)
+def validate_entry(date, blood_sugar):
+    # Add validation logic here (e.g., date format, blood sugar range)
+    return True
 
-    return redirect(url_for("index"))
 
-# Function to plot the data
+def add_entry(date_str, blood_sugar):
+    global ENTRIES
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+    entry = {"Date": date, "Blood Sugar (mg/dL)": blood_sugar}
+    ENTRIES.append(entry)
+
+    df = pd.DataFrame(ENTRIES)
+    df.to_csv(CSV_FILE, index=False)
+
+
 def plot_data():
-    if not entries:
+    if not ENTRIES:
         return None
-    df = pd.DataFrame(entries)
+    df = pd.DataFrame(ENTRIES)
     df.set_index("Date", inplace=True)
     plt.figure(figsize=(10, 6))
     df.plot(title="Blood Sugar Tracker")
@@ -53,43 +49,41 @@ def plot_data():
     plt.savefig("static/plot.png")
     return "static/plot.png"
 
-# Function to check blood sugar and send notifications
-def check_blood_sugar():
-    while True:
-        blood_sugar_data = get_blood_sugar_data_from_csv(csv_file)  # Get blood sugar data from the CSV
-        last_check_time = get_last_check_time()  # Get the last check time from the module
-
-        # ... Your existing code for notifications and reminders ...
-
-        time.sleep(3600)  # Repeat every hour
-
-# Create a separate thread for the reminder logic
-reminder_thread = threading.Thread(target=check_blood_sugar)
-reminder_thread.daemon = True
-reminder_thread.start()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return add_entry()
-    load_data()
-    plot_image = plot_data()  # Get the plot image filename
-    return render_template("index.html", entries=entries, plot_image=plot_image)
+        date_str = request.form.get("date")
+        blood_sugar = float(request.form.get("blood_sugar"))
+
+        if validate_entry(date_str, blood_sugar):
+            add_entry(date_str, blood_sugar)
+            return redirect(url_for("index"))
+
+    plot_image = plot_data()
+    return render_template("index.html", entries=ENTRIES, plot_image=plot_image)
+
 
 @app.route("/clear_data")
 def clear_data():
-    global entries
-    entries = []
-    # Clear the CSV file by overwriting it with an empty DataFrame
+    global ENTRIES
+    ENTRIES = []
     df = pd.DataFrame(columns=["Date", "Blood Sugar (mg/dL)"])
-    df.to_csv(csv_file, index=False)
+    df.to_csv(CSV_FILE, index=False)
     return redirect(url_for("index"))
+
 
 @app.route("/previous_results")
 def previous_results():
-    load_data()  # Load the data
-    return render_template("previous_results.html", entries=entries)
+    return render_template("previous_results.html", entries=ENTRIES)
+
+
+@app.route("/chart")
+def chart():
+    plot_image = plot_data()
+    return render_template("chart.html", plot_image=plot_image)
+
 
 if __name__ == "__main__":
-    load_data()  # Call load_data to load existing data
+    load_data()  # Call this only once when the app starts
     app.run(debug=True, port=3000)
