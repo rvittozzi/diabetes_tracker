d422508660e246aceec52cc780f4ca1258f5d63a
rvittozzi <137367134+rvittozzi@users.noreply.github.com>
2023-09-25 01:36:43 -0400

    edited files

--- init_db.py
+++ init_db.py
@@ -1,12 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
+# Function to create a users table in a given database connection
+def create_users_table(cursor):
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT);'''
+    )
 
-# Create table for storing user information
-cursor.execute(
-    '''CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT);'''
-)
+# Connect to the first database (this will create the file if it does not exist)
+conn1 = sqlite3.connect("users.db")
+cursor1 = conn1.cursor()
+# Create users table in the first database
+create_users_table(cursor1)
+# Commit changes and close the first connection
+conn1.commit()
+conn1.close()
 
-conn.commit()
-conn.close()
+# Connect to the second database (this will create the file if it does not exist)
+conn2 = sqlite3.connect("blood_sugar_entry.db")
+cursor2 = conn2.cursor()
+# Create users table in the second database
+create_users_table(cursor2)
+# Commit changes and close the second connection
+conn2.commit()
+conn2.close()
--- main.py
+++ main.py
@@ -1,53 +1,81 @@
-from flask import Flask, render_template, request, redirect, url_for
-import pandas as pd
-import matplotlib.pyplot as plt
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import os
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BloodSugarEntry.db'  # First database
+app.config['SQLALCHEMY_BINDS'] = {
+    'users': 'sqlite:///users.db'  # Second database
+}
+app.secret_key = "hercules"
 
-# Configurations
-CSV_FILE = "diabetes_data.csv"
-ENTRIES = []
+db = SQLAlchemy(app)
 
 
-def load_data():
-    global ENTRIES
-    if os.path.exists(CSV_FILE):
-        try:
-            df = pd.read_csv(CSV_FILE)
-            df["Date"] = pd.to_datetime(df["Date"])
-            ENTRIES.extend(df.to_dict(orient="records"))
-        except Exception as e:
-            print(f"An error occurred while reading the CSV: {e}")
+# First database model
+class BloodSugarEntry(db.Model):
+    __tablename__ = 'blood_sugar_entry'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    blood_sugar = db.Column(db.Float, nullable=False)
+
+
+# Second database model
+
+class User(db.Model):
+    __bind_key__ = 'users'  # Specify the database
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 
 def validate_entry(date, blood_sugar):
-    # Add validation logic here (e.g., date format, blood sugar range)
-    return True
+    return True  # Add validation logic here
 
 
 def add_entry(date_str, blood_sugar):
-    global ENTRIES
     date = datetime.strptime(date_str, "%Y-%m-%d")
-    entry = {"Date": date, "Blood Sugar (mg/dL)": blood_sugar}
-    ENTRIES.append(entry)
-
-    df = pd.DataFrame(ENTRIES)
-    df.to_csv(CSV_FILE, index=False)
+    new_entry = BloodSugarEntry(date=date, blood_sugar=blood_sugar)
+    db.session.add(new_entry)
+    db.session.commit()
 
 
-def plot_data():
-    if not ENTRIES:
+def plot_data(entries):
+    if not entries:
         return None
-    df = pd.DataFrame(ENTRIES)
-    df.set_index("Date", inplace=True)
+
+    dates = [entry.date for entry in entries]
+    blood_sugar_levels = [entry.blood_sugar for entry in entries]
+
     plt.figure(figsize=(10, 6))
-    df.plot(title="Blood Sugar Tracker")
+    plt.plot(dates, blood_sugar_levels)
+    plt.title("Blood Sugar Tracker")
     plt.xlabel("Date")
     plt.ylabel("Blood Sugar (mg/dL)")
     plt.savefig("static/plot.png")
     return "static/plot.png"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "password":
+            session["username"] = username
+            return redirect(url_for("index"))
+        else:
+            flash("Invalid credentials")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -60,30 +88,43 @@ def index():
             add_entry(date_str, blood_sugar)
             return redirect(url_for("index"))
 
-    plot_image = plot_data()
-    return render_template("index.html", entries=ENTRIES, plot_image=plot_image)
+    entries = BloodSugarEntry.query.all()
+    plot_image = plot_data(entries)
+    return render_template("index.html", entries=entries, plot_image=plot_image)
 
 
 @app.route("/clear_data")
 def clear_data():
-    global ENTRIES
-    ENTRIES = []
-    df = pd.DataFrame(columns=["Date", "Blood Sugar (mg/dL)"])
-    df.to_csv(CSV_FILE, index=False)
+    db.session.query(BloodSugarEntry).delete()
+    db.session.commit()
     return redirect(url_for("index"))
 
 
 @app.route("/previous_results")
 def previous_results():
-    return render_template("previous_results.html", entries=ENTRIES)
+    entries = BloodSugarEntry.query.all()
+    return render_template("previous_results.html", entries=entries)
 
 
 @app.route("/chart")
 def chart():
-    plot_image = plot_data()
+    entries = BloodSugarEntry.query.all()
+    plot_image = plot_data(entries)
     return render_template("chart.html", plot_image=plot_image)
 
 
+# Add this line to explicitly set the table name (Optional)
+class BloodSugarEntry(db.Model):
+    __tablename__ = 'blood_sugar_entry'
+    __table_args__ = {'extend_existing': True}  # new line
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    blood_sugar = db.Column(db.Float, nullable=False)
+
+
+# ... (rest of the code remains the same)
+
 if __name__ == "__main__":
-    load_data()  # Call this only once when the app starts
+    with app.app_context():  # Explicitly providing the Flask app context (Optional)
+        db.create_all()
     app.run(debug=True, port=3000)
